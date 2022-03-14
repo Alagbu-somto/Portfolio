4d97e6f3dedb93a4ab99a952962e3d6a5598f931
Alagbu-somto <98598471+Alagbu-somto@users.noreply.github.com>
2022-03-14 08:51:33 +0100

    added interactions

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
+var $animation_elements = $('.animation-element');
+var $animateUp = $('.animate-up');
+var $window = $(window);
 
-$(window).on("load",function(){
-    $("#intro").fadeOut(800).fadeIn(1500);
-});
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+ 
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+ 
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+
+  $.each($animateUp, function() {
+    let $element = $(this);
+    let element_height = $element.outerHeight();
+    let element_top_position = $element.offset().top;
+    let element_bottom_position = (element_top_position + element_height);
+ 
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('view');
+    } else {
+      $element.removeClass('view');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+
